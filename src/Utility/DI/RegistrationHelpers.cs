1162d0048c69fa7a246c97a36021c9ae72cef553
Nikita Tulenev <n1tulenev@gmail.com>
2024-03-16 15:09:58 +0100

    Add RegistrationHelpers

---
+++ src/Utility/DI/RegistrationHelpers.cs
@@ -0,0 +1,21 @@
+﻿using Abstractions;
+using Logic;
+using Microsoft.Extensions.DependencyInjection;
+using Utility.Console;
+
+namespace Utility.DI;
+
+public static class RegistrationHelpers
+{
+    public static void RegisterApp(this IServiceCollection services)
+    {
+        services.AddSingleton<Application>();
+    }
+
+    public static void RegisterReports(this IServiceCollection services)
+    {
+        services.AddSingleton<IGitReportBuilder, GitReportBuilder>();
+        services.AddSingleton<IGitTagsLoader, GitTagsLoader>();
+        services.AddSingleton<IGitReportPrinter, GitReportPrinter>();
+    }
+}
