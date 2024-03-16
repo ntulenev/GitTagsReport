befdff47fef44cb4cccfc5adaee36da991b735ee
Nikita Tulenev <n1tulenev@gmail.com>
2024-03-16 19:56:05 +0100

    Add xmldoc for IGitReportPrinter

--- src/Abstractions/IGitReportPrinter.cs
+++ src/Abstractions/IGitReportPrinter.cs
@@ -2,7 +2,16 @@
 
 namespace Abstractions;
 
+/// <summary>
+/// Defines the contract for a service that prints reports based on Git tag metadata and a specified task filter.
+/// </summary>
 public interface IGitReportPrinter
 {
-    public void Print(IEnumerable<GitTagMetadata> items,TicketKey taskFilter);
+    /// <summary>
+    /// Prints a report for the specified Git tag metadata items, filtered by a ticket key.
+    /// </summary>
+    /// <param name="items">The collection of Git tag metadata to include in the report.</param>
+    /// <param name="taskFilter">The ticket key to filter the report by. Only description related to this ticket key
+    /// should be included.</param>
+    public void Print(IEnumerable<GitTagMetadata> items, TicketKey taskFilter);
 }
