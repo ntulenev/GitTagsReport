f62031845bc5a13d74b783e7657101137a24274b
Nikita Tulenev <n1tulenev@gmail.com>
2024-03-16 15:08:34 +0100

    Add TicketKey

---
+++ src/Models/TicketKey.cs
@@ -0,0 +1,17 @@
+﻿namespace Models;
+
+public sealed class TicketKey
+{
+    public TicketKey(string ticketKey)
+    {
+        if (string.IsNullOrWhiteSpace(ticketKey))
+        {
+            throw new ArgumentException("Tag cannot be null, empty, or whitespace.", nameof(ticketKey));
+            
+        }
+
+        Value = ticketKey;
+    }
+    
+    public string Value { get; }
+}
