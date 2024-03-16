a74ab79d1160bdfe64f1e349dcb0e48e3dc62e0b
Nikita Tulenev <n1tulenev@gmail.com>
2024-03-16 20:00:43 +0100

    Add xmldoc for GitReportBuilder

--- src/Logic/GitReportBuilder.cs
+++ src/Logic/GitReportBuilder.cs
@@ -3,8 +3,19 @@ using Models;
 
 namespace Logic;
 
+/// <summary>
+/// Builds and outputs Git reports based on tag metadata and a specified task filter.
+/// </summary>
 public sealed class GitReportBuilder : IGitReportBuilder
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GitReportBuilder"/>
+    /// class with specified loader and printer services.
+    /// </summary>
+    /// <param name="loader">The service used to load Git tag metadata.</param>
+    /// <param name="printer">The service used to print the report.</param>
+    /// <exception cref="ArgumentNullException">Thrown if either <paramref name="loader"/>
+    /// or <paramref name="printer"/> is null.</exception>
     public GitReportBuilder(IGitTagsLoader loader, IGitReportPrinter printer)
     {
         ArgumentNullException.ThrowIfNull(loader);
@@ -13,7 +24,8 @@ public sealed class GitReportBuilder : IGitReportBuilder
         _loader = loader;
         _printer = printer;
     }
-
+    
+    /// <inheritdoc/>
     public void Build(GitPath path, TicketKey taskFilter)
     {
         var tags = _loader.LoadTags(path);
