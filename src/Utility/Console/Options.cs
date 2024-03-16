3619afcf9307e72a66b19e368465be8727555753
Nikita Tulenev <n1tulenev@gmail.com>
2024-03-16 15:10:06 +0100

    Add Options

---
+++ src/Utility/Console/Options.cs
@@ -0,0 +1,12 @@
+﻿using CommandLine;
+
+namespace Utility.Console;
+
+public sealed class Options
+{
+    [Option('p', "path", Required = true, HelpText = "Path to git directory.")]
+    public required string DirectoryPath { get; init; }
+
+    [Option('k', "key", Required = true, HelpText = "Ticket system Task key.")]
+    public required string TicketKey { get; init; }
+}
