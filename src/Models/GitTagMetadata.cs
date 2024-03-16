fab48685f3ff344e1c4245c91248c67f3bc8a777
Nikita Tulenev <n1tulenev@gmail.com>
2024-03-16 15:08:46 +0100

    Add GitTagMetadata

---
+++ src/Models/GitTagMetadata.cs
@@ -0,0 +1,21 @@
+﻿namespace Models;
+
+public sealed class GitTagMetadata
+{
+    public string Tag { get; }
+
+    public string Description { get; }
+
+    public GitTagMetadata(string tag, string description)
+    {
+        if (string.IsNullOrWhiteSpace(tag))
+        {
+            throw new ArgumentException("Tag cannot be null, empty, or whitespace.", nameof(tag));
+        }
+
+        Tag = tag;
+        Description = description ?? NoDescription;
+    }
+
+    private const string NoDescription = "<No description>";
+}
