a75e5dc2e4713efd79b3d6e55a9e2404608d4819
Nikita Tulenev <n1tulenev@gmail.com>
2024-03-16 19:51:51 +0100

    Add xmldoc for Application

--- src/Utility/Console/Application.cs
+++ src/Utility/Console/Application.cs
@@ -4,13 +4,27 @@ using Models;
 
 namespace Utility.Console;
 
+/// <summary>
+/// Represents the core application class that orchestrates the parsing of command-line arguments
+/// and the execution of the application logic based on those arguments.
+/// </summary>
 public sealed class Application
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Application"/> class with the necessary dependencies.
+    /// </summary>
+    /// <param name="reportBuilder">The report builder used to generate git reports.</param>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="reportBuilder"/> is null.</exception>
     public Application(IGitReportBuilder reportBuilder)
     {
         ArgumentNullException.ThrowIfNull(reportBuilder);
         _reportBuilder = reportBuilder;
     }
+    
+    /// <summary>
+    /// Parses the command-line arguments and executes the application logic based on the provided options.
+    /// </summary>
+    /// <param name="args">The command-line arguments passed to the application.</param>
     public void Run(params string[] args)
     {
         Parser.Default.ParseArguments<Options>(args)
