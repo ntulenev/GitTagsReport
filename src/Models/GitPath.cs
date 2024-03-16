f7a53d064b566470615c1eb94e20f55fd803f3d8
Nikita Tulenev <n1tulenev@gmail.com>
2024-03-16 15:08:24 +0100

    Add GitPath

---
+++ src/Models/GitPath.cs
@@ -0,0 +1,22 @@
+﻿namespace Models;
+
+public sealed class GitPath
+{
+    public GitPath(string path)
+    {
+        if (string.IsNullOrWhiteSpace(path))
+        {
+            throw new ArgumentException("Path cannot be null or whitespace.", nameof(path));
+        }
+
+        if (!Directory.Exists(path))
+        {
+            throw new ArgumentException("The path does not exist or is not a valid file or directory path.",
+                nameof(path));
+        }
+
+        Path = path;
+    }
+
+    public string Path { get; }
+}
