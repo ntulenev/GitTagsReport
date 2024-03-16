38123ed143d3eef907fcb7cb4be4f71b5572acf8
Nikita Tulenev <n1tulenev@gmail.com>
2024-03-16 15:10:44 +0100

    Build app

---
+++ src/Utility/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Utility.Console;
+using Utility.DI;
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.RegisterReports();
+serviceCollection.RegisterApp();
+var provider = serviceCollection.BuildServiceProvider();
+using var serviceScope = provider.CreateScope();
+var scopeProvider = serviceScope.ServiceProvider;
+
+try
+{
+    var app = scopeProvider.GetRequiredService<Application>();
+    app.Run(args);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Application error: {ex.Message}");
+}
