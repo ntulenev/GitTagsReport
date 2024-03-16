64085e6720ec063b32c84a2762cf9ec1f0ac2fe6
Nikita Tulenev <n1tulenev@gmail.com>
2024-03-16 19:55:57 +0100

    Add xmldoc for IGitTagsLoader

--- src/Abstractions/IGitTagsLoader.cs
+++ src/Abstractions/IGitTagsLoader.cs
@@ -2,7 +2,18 @@
 
 namespace Abstractions;
 
+/// <summary>
+/// Defines the contract for a service responsible for loading Git tag metadata from a specified repository.
+/// </summary>
 public interface IGitTagsLoader
 {
+    /// <summary>
+    /// Loads and returns Git tag metadata from the repository located at the specified path.
+    /// </summary>
+    /// <param name="path">The path to the Git repository from which to load tag metadata.</param>
+    /// <returns>
+    /// An enumerable collection of <see cref="GitTagMetadata"/> objects, each representing metadata for a single Git tag
+    /// found in the specified repository.
+    /// </returns>
     public IEnumerable<GitTagMetadata> LoadTags(GitPath path);
 }
