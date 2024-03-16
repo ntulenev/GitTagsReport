7c967c4ef7e0339e42f201822b12a53ca58052e1
Nikita Tulenev <n1tulenev@gmail.com>
2024-03-16 15:09:40 +0100

    Add GitReportPrinter

---
+++ src/Logic/GitReportPrinter.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+using Abstractions;
+using ConsoleTables;
+using Models;
+
+namespace Logic;
+
+public sealed class GitReportPrinter : IGitReportPrinter
+{
+    public void Print(IEnumerable<GitTagMetadata> items, TicketKey ticketKey)
+    {
+        var pattern = new Regex(@$"{ticketKey.Value}-\d+");
+
+        var table = new ConsoleTable("Tag", "Description");
+        foreach (var tag in items)
+        {
+            var taskId = SearchKey(tag.Description);
+            table.AddRow(tag.Tag, taskId);
+        }
+        table.Write();
+
+        string SearchKey(string description) => pattern.Match(description).Value;
+    }
+}
