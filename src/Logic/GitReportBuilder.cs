673c49750ecb8a368887e1b6c0d4e7860fa14e09
Nikita Tulenev <n1tulenev@gmail.com>
2024-03-16 15:09:47 +0100

    Add GitReportBuilder

---
+++ src/Logic/GitReportBuilder.cs
@@ -0,0 +1,25 @@
+﻿using Abstractions;
+using Models;
+
+namespace Logic;
+
+public sealed class GitReportBuilder : IGitReportBuilder
+{
+    public GitReportBuilder(IGitTagsLoader loader, IGitReportPrinter printer)
+    {
+        ArgumentNullException.ThrowIfNull(loader);
+        ArgumentNullException.ThrowIfNull(printer);
+
+        _loader = loader;
+        _printer = printer;
+    }
+
+    public void Build(GitPath path, TicketKey taskFilter)
+    {
+        var tags = _loader.LoadTags(path);
+        _printer.Print(tags,taskFilter);
+    }
+
+    private readonly IGitTagsLoader _loader;
+    private readonly IGitReportPrinter _printer;
+}
