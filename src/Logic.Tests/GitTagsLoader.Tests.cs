06a46968d05d36c420644667d546c5a44de91748
Nikita Tulenev <n1tulenev@gmail.com>
2024-04-17 17:33:32 +0200

    Add GitTagsLoader Tests

---
+++ src/Logic.Tests/GitTagsLoader.Tests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using LibGit2Sharp;
+using Moq;
+
+namespace Logic.Tests;
+
+public class GitTagsLoaderTests
+{
+    [Fact(DisplayName = nameof(GitTagsLoader) + " throws ArgumentNullException when repoFactory is null")]
+    [Trait("Category", "Unit")]
+    public void ConstructorThrowsArgumentNullExceptionWhenRepoFactoryIsNull()
+    {
+        // Act
+        Action act = () => _ = new GitTagsLoader(null!);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact(DisplayName = "LoadTags throws ArgumentNullException when path is null")]
+    [Trait("Category", "Unit")]
+    public void LoadTagsThrowsArgumentNullExceptionWhenPathIsNull()
+    {
+        // Arrange
+        var loader = new GitTagsLoader(_ => new Mock<IRepository>(MockBehavior.Strict).Object);
+
+        // Act
+        var ex = Record.Exception(() => _ = loader.LoadTags(null!).ToList());
+
+        // Assert
+        ex.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+    }
+}
