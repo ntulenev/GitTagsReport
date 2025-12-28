c1263b8497d5d81c28098a2387a8189d40f8e8b8
Nikita Tulenev <n1tulenev@gmail.com>
2025-12-28 08:37:52 +0100

    Adds global suppressions for code style issues
    Adds a global suppressions file to suppress specific code style warnings (IDE0058) in the test project.
    
    These suppressions address instances where expression values are not used, primarily in test methods. This prevents noise in the code analysis results while maintaining the functionality of the tests.

---
+++ src/Utility.Tests/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0058:Expression value is never used", Justification = "<Pending>", Scope = "member", Target = "~M:Utility.Tests.ApplicationTests.ConstructorThrowsArgumentNullExceptionIfGitReportBuilderIsNull")]
+[assembly: SuppressMessage("Style", "IDE0058:Expression value is never used", Justification = "<Pending>", Scope = "member", Target = "~M:Utility.Tests.ApplicationTests.ApplicationCanBeCreatedWithValidParams")]
+[assembly: SuppressMessage("Style", "IDE0058:Expression value is never used", Justification = "<Pending>", Scope = "member", Target = "~M:Utility.Tests.ApplicationTests.RunHandlesInvalidCommandLineArguments(System.String,System.String)")]
+[assembly: SuppressMessage("Style", "IDE0058:Expression value is never used", Justification = "<Pending>", Scope = "member", Target = "~M:Utility.Tests.ApplicationTests.RunExecutesReportBuildSuccessfullyWithValidArguments")]
